99bfd215b6af69cd9e045bb1f954a13754b08409
Raza ul Mustafa <razaulmustafa898@gmail.com>
2024-12-03 21:11:30 +0500

    feat: solve fixes

--- backend/controllers/chats/getChat.controller.js
+++ backend/controllers/chats/getChat.controller.js
@@ -2,13 +2,28 @@ const Chat = require("../../models/Chat.js");
 
 async function getAllChats(req, res) {
   try {
-    const chats = await Chat.find(); // Use the imported model
-    res.status(200).json(chats); // Send the chats as a response
-    console.log("fetch All Chats");
+    const chats = await Chat.find();
+    const checkSuccess = res.status(200);
+    const checkError = res.status(500) || res.status(404);
+    if (checkSuccess) {
+      checkSuccess.json({
+        error: false,
+        message: "Chats fetched successfully",
+        chats: chats,
+      });
+    }
+    if (checkError) {
+      checkError.json({
+        error: true,
+        message: "Error fetching chats",
+      });
+    }
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error fetching chats", error });
+    res.status(500).json({
+      error: true,
+      message: "Error fetching chats",
+      error,
+    });
   }
 }
 
--- backend/controllers/homeProfiles/getHomeProfiles.controller.js
+++ backend/controllers/homeProfiles/getHomeProfiles.controller.js
@@ -4,16 +4,23 @@ async function getHomeProfiles(req, res) {
   try {
     const fetchHomeProfiles = await homeProfiles.find();
     const checkSuccess = res.status(200);
+    const checkError = res.status(500) || res.status(404);
     if (checkSuccess) {
-      checkSuccess.json(fetchHomeProfiles);
-    } else {
-      res
-        .status(500)
-        .json({ message: `Failed to fetch home profiles` });
+      checkSuccess.json({
+        error: false,
+        message: "Home profiles fetched successfully",
+        homeProfiles: fetchHomeProfiles,
+      });
     }
-    // res.status(200).json(fetchHomeProfiles);
+    if (checkError) {
+      checkError.json({
+        error: true,
+        message: `Failed to fetch home profiles`,
+      });
+    }
   } catch (error) {
     res.status(500).json({
+      error: true,
       message: `Error fetching home profiles`,
       error,
     });
